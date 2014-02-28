6c43e9c9ee3e1557d744c90fe1bee3fccc6263a7
Alex Mai <alexandertmai@gmail.com>
2014-02-28 14:18:45 -0800

    Changed Script.js and HTML
    Yeah. Added Animation Structure.

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-$(document).ready(function() {
-    $("button").click(function() {
-        $("#push").animate({
-            left: "250px"
-        }, fast);
+var nucleo = [
+    {
+        x: 200px;
+        y: 200px;
+        n: "A";
+    },
+    {
+        x: 250px;
+        y: 200px;
+        n: "T";
+    }
+];
+$(document).ready(function(){
+    
+    $("button").click(function(){
+        $("#push1").animate({left:'250px'});
+        $("#push2").animate({left:'250px'});
     });
 });
