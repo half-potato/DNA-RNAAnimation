676afc8fcb9a612677b4833b8101f89fbd970015
Alex Mai <alexandertmai@gmail.com>
2014-02-28 14:37:37 -0800

    Fixed Everything

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-$(document).ready(function() {
-    $("button").click(function() {
-        $("#push").animate({
-            left: "250px"
-        }, fast);
+/*var nucleo = [
+    {
+        x: 200px;
+        y: 200px;
+        n: "A";
+    },
+    {
+        x: 250px;
+        y: 200px;
+        n: "T";
+    }
+];*/
+$(document).ready(function(){
+    
+    $("button").click(function(){
+        $("adenine").animate({left:'250px'});
     });
 });
