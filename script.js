00b1eebd64e3599f1ec09c11fcd0580e4bf9f660
fhebert-perkins <finley.hebert@gmail.com>
2014-02-28 21:24:28 -0800

    Anti sense is now the same as sense

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 var sense = [{n: "A"}, {n: "T"}, {n: "G"}, {n: "C"}];
+var antisense = [{n: "A"}, {n: "T"}, {n: "G"}, {n: "C"}];
 $(document).ready(function () {
     for (var i = 0; i < sense.length; i++) {
         switch(sense[i].n) {
@@ -20,7 +21,32 @@ $(document).ready(function () {
                 break;
         }
     }
+    for (var i = 0; i < antisense.length; i++){
+        switch(antisense[i].n){
+            case "A":
+                $(".antisense").append("<div id = '" + i + "' style ='left:"+ 50 * i + "px; top: 160px;' class = \"adenine\">A</div>");
+                break;
+            case "T":
+                $(".antisense").append("<div id = '" + i + "' style ='left:"+ 50 * i + "px; top: 160px;' class = \"thymine\">T</div>");
+                break;
+            case "G":
+                $(".antisense").append("<div id = '" + i + "' style ='left:"+ 50 * i + "px; top: 160px;' class = \"guanine\">G</div>");
+                break;
+            case "C":
+                $(".antisense").append("<div id = '" + i + "' style ='left:"+ 50 * i + "px; top: 160px;' class = \"cytosine\">C</div>");
+                break;
+            case "U":
+                $(".antisense").append("<div id = '" + i + "' style ='left:"+ 50 * i + "px; top: 160px;' class = \"adenine\">A</div>");
+                break;
+        }
+            
+    }
     $("button").click(function() {
-        $("#"+i).animate({top:"150px"});
+        for(var i = 0; i < sense.length; i++){
+            $(".sense, #"+i).animate({top:"-150px"}, 500);
+        }
+        for(var i = 0; i < antisense.length; i++){
+            $(".antisense, #"+i).animate({top:"+150px"}, 500);
+        }
     });
 });
