c898e08b45aa412a3b2f24aed8e92106edb5f7a2
artiespeedy <alexandertmai@gmail.com>
2014-03-01 14:10:56 -0800

    Animation Engine... DONE!

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 
-var sense = [{n: "A"}, {n: "G"}, {n: "G"}, {n: "T"}, {n: "C"}, {n: "T"}, {n: "G"}, {n: "A"}, {n: "A"}, {n: "T"}, {n: "C"}];
-var animations = {step1: [{id: "#1", x: "+=0px", y: "+=15px"}], step2: [{id: "#2", x: "+=0px", y: "+=30px"}, {id: "#3", x: "+=0px", y: "+=30px"}]};
+var     sense = [{n: "A"}, {n: "G"}, {n: "G"}, {n: "T"}, {n: "C"}, {n: "T"}, {n: "G"}, {n: "A"}, {n: "A"}, {n: "T"}, {n: "C"}];
+var antisense = [{n: "T"}, {n: "C"}, {n: "C"}, {n: "A"}, {n: "G"}, {n: "A"}, {n: "C"}, {n: "T"}, {n: "T"}, {n: "A"}, {n: "G"}];
+var animations = [[["#1", "+=0px", "-=15px"]], [["#2", "+=0px", "-=30px"], ["#3", "+=0px", "-=30px"]]];
 
 function arrayAnimate(animations) {
-    var step = animations.shift(), short = animations, object, directions;
-    if(animations.length;() == 0) {
+    var step, short = animations, object, directions, n;
+
+    if (animations.length != 0) {
+        step = animations.shift();
+        console.log(step);
+        for (n = 0; n < step.length; n++) {
+        console.log(step[n]);
+        if(n == step.length - 1) {
+            console.log("Hi");
+            $(step[n][0]).animate( {top: step[n][2], left: step[n][1]}, arrayAnimate(animations));
+        } else {
+            console.log("Hi");
+            $(step[n][0]).animate( {top: step[n][2], left: step[n][1]});
+        }
+    }
+    }
+
+    /*for(var i = 0; i < step.length; i++) {
+        console.log(step[i][0] + step[i][1] + step[i][2]);
+        console.log(i);
+        $(step[i][0]).animate(
+          {left: step[i][2],
+           top: step[i][3]});
+    }*/
+
+    /*if(animations.length == 0) {
         for (object in step) {
             console.log(object.id);
             $(object.id).animate(
@@ -14,14 +39,11 @@ function arrayAnimate(animations) {
         for (object in step) {
             console.log(object.id);
             $(object.id).animate(
-                {top: object.y, left: object.x}, 
+                {top: object.y, left: object.x},
                 arrayAnimate(short));
         }
-    }
+    }*/
 }
-
-var sense = [{n: "A"}, {n: "T"}, {n: "G"}, {n: "C"}];
-var antisense = [{n: "A"}, {n: "T"}, {n: "G"}, {n: "C"}];
 
 $(document).ready(function () {
     for (var i = 0; i < sense.length; i++) {
@@ -43,9 +65,8 @@ $(document).ready(function () {
                 break;
         }
     }
-    
+
     $("button").click(function () {
-        console.log("hi");
         arrayAnimate(animations);
         /*$("#1").animate({top:"-=15px"}, function () {
             $("#2").animate({top:"-=30px"}, function () {
@@ -54,14 +75,14 @@ $(document).ready(function () {
                         $("#5").animate({top:"-=50px"}, function () {
                             $("#6").animate({top:"-=50px"}, function () {
                                 $("#7").animate({top:"-=50px"}, function () {
-                                    
+
                                 });
                             });
                         });
                     });
                 });
-            });
-        });*/
+            });*/
+    });
     for (var i = 0; i < antisense.length; i++){
         switch(antisense[i].n){
             case "A":
@@ -80,14 +101,14 @@ $(document).ready(function () {
                 $(".antisense").append("<div id = '" + i + "' style ='left:"+ 50 * i + "px; top: 160px;' class = \"adenine\">A</div>");
                 break;
         }
-            
+
     }
-    $("button").click(function() {
+    /*$("button").click(function() {
         for(var i = 0; i < sense.length; i++){
             $(".sense, #"+i).animate({top:"-150px"}, 500);
         }
         for(var i = 0; i < antisense.length; i++){
             $(".antisense, #"+i).animate({top:"+150px"}, 500);
         }
-    });
+    });*/
 });
