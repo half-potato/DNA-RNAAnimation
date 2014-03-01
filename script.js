366eea89b68b05fc9d833e9239222353760e5af4
artiespeedy <alexandertmai@gmail.com>
2014-02-28 22:29:49 -0800

    My hands are typing words!
    Added animation engine.

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 
-var sense = [{n: "A"}, {n: "T"}, {n: "G"}, {n: "C"}];
+var sense = [{n: "A"}, {n: "G"}, {n: "G"}, {n: "T"}, {n: "C"}, {n: "T"}, {n: "G"}, {n: "A"}, {n: "A"}, {n: "T"}, {n: "C"}];
+var animations = {step1: [{id: "#1", x: "+=0px", y: "+=15px"}], step2: [{id: "#2", x: "+=0px", y: "+=30px"}, {id: "#3", x: "+=0px", y: "+=30px"}]};
+
+function arrayAnimate(animations) {
+    var step = animations.shift(), short = animations, object, directions;
+    if(animations.length == 0) {
+        for (object in step) {
+            console.log(object.id);
+            $(object.id).animate(
+                {top: object.y, left: object.x});
+        }
+    } else {
+        for (object in step) {
+            console.log(object.id);
+            $(object.id).animate(
+                {top: object.y, left: object.x}, 
+                arrayAnimate(short));
+        }
+    }
+}
+
 $(document).ready(function () {
     for (var i = 0; i < sense.length; i++) {
         switch(sense[i].n) {
@@ -20,7 +40,24 @@ $(document).ready(function () {
                 break;
         }
     }
-    $("button").click(function() {
-        $("#"+i).animate({top:"150px"});
+    
+    $("button").click(function () {
+        console.log("hi");
+        arrayAnimate(animations);
+        /*$("#1").animate({top:"-=15px"}, function () {
+            $("#2").animate({top:"-=30px"}, function () {
+                $("#3").animate({top:"-=45px"}, function () {
+                    $("#4").animate({top:"-=50px"}, function () {
+                        $("#5").animate({top:"-=50px"}, function () {
+                            $("#6").animate({top:"-=50px"}, function () {
+                                $("#7").animate({top:"-=50px"}, function () {
+                                    
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });*/
     });
 });
