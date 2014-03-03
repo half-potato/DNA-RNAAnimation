3bf3f292581e878bccb81bd6035222f7d509f427
Alex Mai <alexandertmai@gmail.com>
2014-03-03 12:20:28 -0800

    My Hands are typing words! WORDS!

--- script.js
+++ script.js
@@ -10,18 +10,26 @@ var antisense = [{n: "T"}, {n: "C"}, {n: "C"}, {n: "A"}, {n: "G"}, {n: "A"}, {n:
                   [["#8", "+=0px", "-=30px"], ["#helicase", "+=50px", "+=0px"]],
                   [["#9", "+=0px", "-=15px"], ["#helicase", "+=50px", "+=0px"]]];*/
                 //<id/class> <x>     <y>
-var animations = [[["#1", "+=0px", "-=15px"]],
-                  [["#2", "+=0px", "-=30px"]],
-                  [["#3", "+=0px", "-=45px"]],
-                  [["#4", "+=0px", "-=50px"]],
-                  [["#5", "+=0px", "-=50px"]],
-                  [["#6", "+=0px", "-=50px"]],
-                  [["#7", "+=0px", "-=45px"]],
-                  [["#8", "+=0px", "-=30px"]],
-                  [["#9", "+=0px", "-=15px"]],
-                  [[".helicase", "+=500px", "+=0px"]],
-                  [[".sense", "+=0", "+=0", "hide"]],
-                  [[".helicase", "+=0", "+=0", "hide"]]];
+var animations = [[[".helicase", "+=50px", "+=0px"]],
+                  [["#1", "+=0px", "-=15px"], ["#a1", "+=0px", "+=15px"]],
+                  [[".helicase", "+=50px", "+=0px"]],
+                  [["#2", "+=0px", "-=30px"], ["#a2", "+=0px", "+=30px"]],
+                  [[".helicase", "+=50px", "+=0px"]],
+                  [["#3", "+=0px", "-=45px"], ["#a3", "+=0px", "+=45px"]],
+                  [[".helicase", "+=50px", "+=0px"]],
+                  [["#4", "+=0px", "-=50px"], ["#a4", "+=0px", "+=50px"]],
+                  [[".helicase", "+=50px", "+=0px"]],
+                  [["#5", "+=0px", "-=50px"], ["#a5", "+=0px", "+=50px"]],
+                  [[".helicase", "+=50px", "+=0px"]],
+                  [["#6", "+=0px", "-=50px"], ["#a6", "+=0px", "+=50px"]],
+                  [[".helicase", "+=50px", "+=0px"]],
+                  [["#7", "+=0px", "-=45px"], ["#a7", "+=0px", "+=45px"]],
+                  [[".helicase", "+=50px", "+=0px"]],
+                  [["#8", "+=0px", "-=30px"], ["#a8", "+=0px", "+=30px"]],
+                  [[".helicase", "+=50px", "+=0px"]],
+                  [["#9", "+=0px", "-=15px"], ["#a9", "+=0px", "+=15px"]],
+                  [[".helicase", "+=50px", "+=0px"]],
+                  [[".helicase", "+=0", "+=0", "hide"], [".polymerase", "+=0", "+=0", "unhide"]]];
         
 
 function arrayAnimate(animations) {
@@ -31,14 +39,42 @@ function arrayAnimate(animations) {
         step = animations.shift();
         for (n = 0; n < step.length; n += 1) {
             if (n === step.length - 1) {
-                if (step[n][3] ==="hide"){
+                if (step[n][3] === "hide"){
                     $(step[n][0]).hide(2500);
-                }
-                else{
-                $(step[n][0]).animate({top: step[n][2], left: step[n][1]},1000, arrayAnimate(animations));
+                } else if(step[n][3] === "unhide") {
+                    $(step[n][0]).show(2500);
+                } else {
+                    $(step[n][0]).animate( {top: step[n][2], left: step[n][1]}, function() {
+                        setTimeout(1);
+                        arrayAnimate(animations);
+                    });
                 }
             } else {
                 $(step[n][0]).animate({top: step[n][2], left: step[n][1]},1000);
+            }
+        }
+    }
+}
+
+function arrayAnimate(animations) {
+    var step, short = animations, n;
+
+    if (animations.length != 0) {
+        step = animations.shift();
+        for (n = 0; n < step.length; n++) {
+            if(n == step.length - 1) {
+                if (step[n][3] ==="hide"){
+                    $(step[n][0]).hide(2500);
+                    arrayAnimate(animations);
+                }
+                else{
+                    $(step[n][0]).animate( {top: step[n][2], left: step[n][1]}, function() {
+                        setTimeout(1);
+                        arrayAnimate(animations);
+                    });
+                }
+            } else {
+                $(step[n][0]).animate( {top: step[n][2], left: step[n][1]});
             }
         }
     }
@@ -72,19 +108,19 @@ $(document).ready(function () {
     for (var i = 0; i < antisense.length; i++){
         switch(antisense[i].n){
             case "A":
-                $(".antisense").append("<div id = a'" + i + "' style ='left:"+ 50 * i + "px; top: 160px;' class = \"adenine\">A</div>");
+                $(".antisense").append("<div id = 'a" + i + "' style ='left:"+ 50 * i + "px; top: 160px;' class = \"adenine\">A</div>");
                 break;
             case "T":
-                $(".antisense").append("<div id = a'" + i + "' style ='left:"+ 50 * i + "px; top: 160px;' class = \"thymine\">T</div>");
+                $(".antisense").append("<div id = 'a" + i + "' style ='left:"+ 50 * i + "px; top: 160px;' class = \"thymine\">T</div>");
                 break;
             case "G":
-                $(".antisense").append("<div id = a'" + i + "' style ='left:"+ 50 * i + "px; top: 160px;' class = \"guanine\">G</div>");
+                $(".antisense").append("<div id = 'a" + i + "' style ='left:"+ 50 * i + "px; top: 160px;' class = \"guanine\">G</div>");
                 break;
             case "C":
-                $(".antisense").append("<div id = a'" + i + "' style ='left:"+ 50 * i + "px; top: 160px;' class = \"cytosine\">C</div>");
+                $(".antisense").append("<div id = 'a" + i + "' style ='left:"+ 50 * i + "px; top: 160px;' class = \"cytosine\">C</div>");
                 break;
             case "U":
-                $(".antisense").append("<div id = a'" + i + "' style ='left:"+ 50 * i + "px; top: 160px;' class = \"adenine\">A</div>");
+                $(".antisense").append("<div id = 'a" + i + "' style ='left:"+ 50 * i + "px; top: 160px;' class = \"adenine\">A</div>");
                 break;
         }
     }
