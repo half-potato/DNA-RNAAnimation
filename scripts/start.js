46208b155e4e7ddf9bbc08f2c5722e03c18c6037
Alex Mai <alexandertmai@gmail.com>
2014-03-07 15:20:47 -0800

    Fix git stuff now

--- scripts/start.js
+++ scripts/start.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("body").append("<div id=\"container\"></div>")
     $("body").append("<div id=\"header\"><h1>DNA Transcription and Translation</h1></div><br><center><div id=\"button\">&#9658; Play<br><span>By Alex Mai &amp; Finley Hebert-Perkins</span></div></center>")
-    $("body").append("<audio class = \"audio1\" preload=\"none\"><source src = \"media/Hello.ogg\" type = \"audio/ogg\">Your browser does not support this audio format.</audio>")
+    //$("body").append("<audio class = \"audio1\" preload=\"none\"><source src = \"media/Hello.ogg\" type = \"audio/ogg\">Your browser does not support this audio format.</audio>")
     
 });
