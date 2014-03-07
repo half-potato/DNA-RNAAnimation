0fffbca00362f0c25716dc3199c38dd8514428ba
Alex Mai <alexandertmai@gmail.com>
2014-03-07 15:27:45 -0800

    Fixed Git

---
+++ scripts/start.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $("body").append("<div id=\"container\"></div>");
+    $("body").append("<div id=\"header\"><h1>DNA Transcription and Translation</h1></div><br><center><div id=\"button\">&#9658; Play<br><span>By Alex Mai &amp; Finley Hebert-Perkins</span></div></center>");
+}
